94f0458c1595af9bb14baa08a00c47e2c411ba95
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-25 02:06:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,7 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
   
-  def self.create(name, breed)
+  def self.create(name:, breed:)
     dog = Dog.new(name: name, breed: breed)
   end
 end
